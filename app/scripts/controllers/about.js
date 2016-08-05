f30ac0415e93413122c13b7441a3d26380c9929b
Stéphane Bouget <bouget.stephane@gmail.com>
2016-08-05 09:57:05 +0200

    initial import yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name populllarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the populllarApp
+ */
+angular.module('populllarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name populllarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the populllarApp
+ */
+angular.module('populllarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
